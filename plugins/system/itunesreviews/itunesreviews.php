20e7b57f088897106c5fbae49336ba68505f2986
Viet Vu <jooservices@gmail.com>
2017-12-13 15:41:06 +0700

    + iTunes reviews

---
+++ plugins/system/itunesreviews/itunesreviews.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package     ItunesReviews
+ * @subpackage  Plugin
+ *
+ * @copyright   Copyright (C) 2008 - 2017 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * ItunesReviews
+ *
+ * @package     System.ItunesReviews
+ * @subpackage  System
+ * @since       1.0.0
+ */
+class PlgSystemItunesreviews extends JPlugin
+{
+	/**
+	 * Auto load language
+	 *
+	 * @var boolean
+	 */
+	protected $autoloadLanguage = true;
+}
