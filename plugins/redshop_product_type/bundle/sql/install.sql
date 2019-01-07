b538068ee6d7f6a7fc42d980728b075063b00e44
hungnguyen <hungnguyen.ckc@gmail.com>
2019-01-07 11:31:49 +0700

    REDSHOP-5247 Plugin Bundle can't install with REDSHOP

--- plugins/redshop_product_type/bundle/sql/install.sql
+++ plugins/redshop_product_type/bundle/sql/install.sql
@@ -14,5 +14,5 @@ CREATE TABLE IF NOT EXISTS `#__redshop_order_bundle` (
   PRIMARY KEY (`order_item_id`,`bundle_id`,`product_id`,`property_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `#__redshop_template` (`template_name`, `template_section`, `template_desc`, `order_status`, `payment_methods`, `published`, `shipping_methods`, `checked_out`, `checked_out_time`) VALUES
-	('bundle', 'bundle_template', '<div class="attribute_listing table-responsive"><table class="table table-striped"><thead><tr><th>{property_number_lbl}</th><th>{property_name_lbl}</th><th></th><th> </th></tr></thead><tbody><tr><td>{bundle_number}</td><td>{bundle_name}</td><td align="center">{bundle_stock_amount_image}</td><td>{bundle_select}</td></tr><tr><td>{property_number}</td><td>{property_name}</td><td align="center">{property_stock_image}</td><td>{property_select}</td></tr></tbody></table></div>', '', '', 1, '', 0, '0000-00-00 00:00:00');
+INSERT INTO `#__redshop_template` (`name`, `section`, `file_name`, `order_status`, `payment_methods`, `published`, `shipping_methods`, `checked_out`, `checked_out_time`, `created_by`, `created_time`, `modified_by`, `modified_date`) VALUES
+	('bundle', 'bundle_template', 'bundle_template', '', '', 1, '', 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00');
