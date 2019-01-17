b7ab1051359401b88053d49d21df861568184094
hungnguyen <hungnguyen.ckc@gmail.com>
2019-01-17 15:34:14 +0700

    REDSHOP-5232 OGman redSHOP plugins need to update category . product

--- plugins/logman/redshop/redshop.php
+++ plugins/logman/redshop/redshop.php
@@ -45,6 +45,84 @@ class PlgLogmanRedshop extends ComLogmanPluginJoomla
 	}
 
 	/**
+	 * Trigger after saved product
+	 *
+	 * @param   array  $data
+	 *
+	 * @param   array  $product_id  product id
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0.0
+	 */
+	public function onAfterProductFullSave($data, $product_id)
+	{
+		$name   = JText::_('COM_REDSHOP_REDSHOP');
+		$type   = 'product';
+		$result = 'changed';
+
+		if ($product_id == 0)
+		{
+			$result = 'addad';
+		}
+
+		$user   = JFactory::getUser();
+		$this->log(
+			array(
+				'object' => array(
+					'package' => $this->_getPackage(),
+					'type'    => $type,
+					'id'      => $this->_getUniqueId($data->product_id),
+					'name'    => $data->product_name,
+					'metadata'=> $data,
+				),
+				'verb'   => 'save',
+				'actor'  => $user->id,
+				'result' => $result
+			)
+		);
+	}
+
+	/**
+	 * Trigger after saved category
+	 *
+	 * @param   array  $data
+	 *
+	 * @param   array  $catID  category id
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0.0
+	 */
+	public function onAfterCategorySave($data, $catID)
+	{
+		$name   = JText::_('COM_REDSHOP_REDSHOP');
+		$type   = 'category';
+		$result = 'changed';
+
+		if ($catID == 0)
+		{
+			$result = 'addad';
+		}
+
+		$user   = JFactory::getUser();
+		$this->log(
+			array(
+				'object' => array(
+					'package' => $this->_getPackage(),
+					'type'    => $type,
+					'id'      => $this->_getUniqueId($data->id),
+					'name'    => $data->name,
+					'metadata'=> $data,
+				),
+				'verb'   => 'save',
+				'actor'  => $user->id,
+				'result' => $result
+			)
+		);
+	}
+
+	/**
 	 * Get package
 	 *
 	 * @return  string
