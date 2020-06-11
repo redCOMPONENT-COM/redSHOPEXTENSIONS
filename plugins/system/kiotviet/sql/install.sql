abaf576c9e56f669738802a14e5ccab6ede0ffef
hungnguyen <hung@redweb.dk>
2020-06-11 09:56:43 +0700

    update code

--- plugins/system/kiotviet/sql/install.sql
+++ plugins/system/kiotviet/sql/install.sql
@@ -19,6 +19,6 @@ ALTER TABLE `#__kiotviet_category_mapping`
   ADD UNIQUE KEY `rs_category_id` (`rs_category_id`);
 COMMIT;
 
-INSERT IGNORE INTO `#__redshop_fields` (`title`, `name`, `type`, `description`, `class`, `section`, `maxlength`, `cols`, `rows`, `size`, `show_in_front`, `required`, `published`, `publish_up`, `publish_down`, `display_in_product`, `ordering`, `display_in_checkout`, `checked_out`, `checked_out_time`, `created_date`, `created_by`, `modified_date`, `modified_by`)
+INSERT IGNORE INTO `#__redshop_fields` (`title`, `name`, `type`, `desc`, `class`, `section`, `maxlength`, `cols`, `rows`, `size`, `show_in_front`, `required`, `published`, `publish_up`, `publish_down`, `display_in_product`, `ordering`, `display_in_checkout`, `checked_out`, `checked_out_time`, `created_date`, `created_by`, `modified_date`, `modified_by`)
 VALUES
 	('Kiotviet Orders', 'rs_kiotviet_orders', '1', '', '', '20', 0, 0, 0, 0, 0, 1, 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 7, 1, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', NULL, '0000-00-00 00:00:00', NULL);
