5307f0b885d3510acc6cddcdf6936c5dc71d2c3d
Ronni <ronni@print.dk>
2023-01-24 19:49:54 +0100

    Make url abolute. Better for Google.

--- plugins/redshop_product/canonical/canonical.php
+++ plugins/redshop_product/canonical/canonical.php
@@ -19,61 +19,52 @@ JLoader::import('redshop.library');
  */
 class PlgRedshop_ProductCanonical extends JPlugin
 {
-	/**
-	 * Example prepare redSHOP Product method
-	 *
-	 * Method is called by the product view
-	 *
-	 * @param   string  &$template  The Product Template Data
-	 * @param   object  &$params    The product params
-	 * @param   object  $product    The product object
-	 *
-	 * @return  null
-	 */
-	public function onPrepareProduct(&$template, &$params, $product)
-	{
-		$app = JFactory::getApplication();
-		$layout = $app->input->getCmd('layout', 'default');
+    /**
+     * Example prepare redSHOP Product method
+     *
+     * Method is called by the product view
+     *
+     * @param   string  &$template  The Product Template Data
+     * @param   object  &$params    The product params
+     * @param   object  $product    The product object
+     *
+     * @return  null
+     */
+    public function onPrepareProduct(&$template, &$params, $product)
+    {
+        $app = JFactory::getApplication();
+        $layout = $app->input->getCmd('layout', 'default');
 
-		// Create canonical url only for default layout.
-		if ('default' !== $layout)
-		{
-			return;
-		}
+        // Create canonical url only for default layout.
+        if ('default' !== $layout) {
+            return;
+        }
 
-		$url = '';
+        $url = '';
 
-		// Check SELF Canonical URL first
-		if ('' != trim($product->canonical_url))
-		{
-			$url = $product->canonical_url;
-		}
-		// Looking for Parent product to set as canonical.
-		elseif ((int) $product->product_parent_id)
-		{
-			$parentProduct = \Redshop\Product\Product::getProductById($product->product_parent_id);
+        // Check SELF Canonical URL first
+        if ('' != trim($product->canonical_url)) {
+            $url = $product->canonical_url;
+        // Looking for Parent product to set as canonical.
+        } elseif ((int) $product->product_parent_id) {
+            $parentProduct = \Redshop\Product\Product::getProductById($product->product_parent_id);
 
-			$url = 'index.php?option=com_redshop&view=product&layout=detail'
-					. '&Itemid=' . $app->input->getInt('Itemid', 0)
-					. '&pid=' . $product->product_parent_id;
+            $url = 'index.php?option=com_redshop&view=product&layout=detail'
+                    . '&Itemid=' . $app->input->getInt('Itemid', 0)
+                    . '&pid=' . $product->product_parent_id;
 
-			if ('' != trim($parentProduct->canonical_url))
-			{
-				$url = $parentProduct->canonical_url;
-			}
-		}
+            if ('' != trim($parentProduct->canonical_url)) {
+                $url = $parentProduct->canonical_url;
+            }
+        }
 
-		// Only add URL to canonical if available
-		if (!empty($url))
-		{
-			if (JURI::isInternal($url))
-			{
-				$url = JURI::root() . $url;
-			}
+        // Only add URL to canonical if available
+        if (!empty($url)) {
+            $url = JURI::root() . $url;
 
-			JFactory::getDocument()->addCustomTag(
-				'<link rel="canonical" href="' . $url . '" />'
-			);
-		}
-	}
+            JFactory::getDocument()->addCustomTag(
+                '<link rel="canonical" href="' . $url . '" />'
+            );
+        }
+    }
 }
